e438d05ebb5b0f40e79c9e382b1c5dbdcf70b5c3
mathaia1 <mathaia1@msu.edu>
2026-07-26 21:41:37 -0400

    More basic java practice

--- Main.java
+++ Main.java
@@ -1,18 +1,22 @@
 public class Main{
     public static void main (String[] args){
-        BankAccount acct = new SavingsAccount(100,25);
-        System.out.println("Withdraw 90 succeeded?" + acct.withdraw(90)); //Won,t succeed as min balance is 25
-        System.out.println("Balance: " + acct.getBalance()); //100
-        System.out.println("Withdraw 50 succeeded?" + acct.withdraw(50)); //Yes
-        System.out.println("Balance: " + acct.getBalance()); // 50
 
-
-
-        Payable[] bills = { new CarLoan(12000, 48), new Subscription(15.99) };
-        double total = 0;
-        for (Payable bill : bills) {
-            total += bill.getMonthlyPayment();
+        for (int i = 0; i<=500; i++){
+            System.out.println((byte) i);
         }
-        System.out.println("Total monthly: " + total);
+       // BankAccount acct = new SavingsAccount(100,25);
+        //System.out.println("Withdraw 90 succeeded?" + acct.withdraw(90)); //Won,t succeed as min balance is 25
+       // System.out.println("Balance: " + acct.getBalance()); //100
+        //System.out.println("Withdraw 50 succeeded?" + acct.withdraw(50)); //Yes
+        //System.out.println("Balance: " + acct.getBalance()); // 50
+
+
+
+        //Payable[] bills = { new CarLoan(12000, 48), new Subscription(15.99) };
+        //double total = 0;
+        //for (Payable bill : bills) {
+         //   total += bill.getMonthlyPayment();
+        //}
+        //System.out.println("Total monthly: " + total);
     }
 }
---
+++ SumSquare.java
@@ -0,0 +1,11 @@
+public class SumSquare{
+    public static void main (String args []){
+        long sum = 0;
+        for (int i =1; i<=30; i++){
+            sum = sum + i*i;
+        }
+
+        System.out.println("The sum of squares of first 30 numbers is " + sum);
+
+    }
+}
